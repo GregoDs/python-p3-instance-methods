646ef0e83dd933fd6c32f4c5e24805a7d459352e
+ <+>
2024-04-28 15:37:47 +0300

    Completed assignments

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,18 @@
 class Person:
     # Class body goes here
 
+
+ 
     #Instance method definition
     pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+#Creating an instance of the class
+
+fido = Person()
+fido.talk()
+fido.walk()
